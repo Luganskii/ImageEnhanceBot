4027db5a335b86974689601272799c82f9a8bfe0
Luganskii <luganchick@example.com>
2024-06-22 21:07:06 +0300

    Add a menu with commands

---
+++ bot-core.py
@@ -0,0 +1,29 @@
+import os
+
+import telebot
+from dotenv import load_dotenv
+from telebot.types import BotCommand
+
+load_dotenv()
+bot = telebot.TeleBot(token=os.getenv('token'))
+
+commands = (
+    BotCommand('start', 'start'),
+    BotCommand('help', 'Show description of the commands and general info'),
+    BotCommand('fix', 'Improve image quality')
+)
+
+bot.set_my_commands(commands)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, 'Hello world')
+
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, 'Help')
+
+
+bot.infinity_polling()
