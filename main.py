2d1908c333ec0e64ebd6e37d267c473708f5aa69
Luganskii <luganchick@example.com>
2024-07-02 16:45:33 +0300

    Add connect to engine

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import subprocess
 
-print('Hello World, here is come text')
+from sqlalchemy import create_engine, text
+
+from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_ECHO
+
+engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=SQLALCHEMY_ECHO)
+
+with engine.connect() as conn:
+    result = conn.execute(text("select 'hello world'"))
+    print(result.all())
 
 subprocess.run(['echo', 'Hello world! We are at \n\r'])
 subprocess.run(['ls', '-l'])
