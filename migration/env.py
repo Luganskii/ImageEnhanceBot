c13d19fd4472d0cec85a05b6b3fd55cc1d98f9a9
Luganskii <luganchick@example.com>
2024-07-06 02:03:51 +0300

    Add Base metadata

--- migration/env.py
+++ migration/env.py
@@ -4,6 +4,8 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 
 from config import SQLALCHEMY_DATABASE_URI
+from models import base
+from models.tables import *  # noqa: F401, F403
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -24,7 +26,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = base.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
