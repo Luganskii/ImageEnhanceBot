bd33b9ebaf6e4d769f6af0f0f87ec60efad32fb3
Luganskii <luganchick@example.com>
2024-07-06 01:09:34 +0300

    Add URL database in alembic.ini

--- migration/env.py
+++ migration/env.py
@@ -3,9 +3,17 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from config import SQLALCHEMY_DATABASE_URI
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+# this will overwrite the ini-file sqlalchemy.url path
+# with the path given in the config of the main code
+
+config.set_main_option('sqlalchemy.url', SQLALCHEMY_DATABASE_URI)
+
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
