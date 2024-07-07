2d48006ce97015815f133b2c690248a8d9ca1f60
Luganskii <luganchick@example.com>
2024-07-07 19:35:55 +0300

    fix misspell

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ def database_url() -> str:
     db_port: int = int(os.getenv('POSTGRES_PORT') or 5432)
     db_user: str = os.getenv('POSTGRES_USERNAME') or 'postgres'
     db_pass: str | None = os.getenv('POSTGRES_PASSWORD')
-    db_name: str = os.getenv('POSTGRES_USERNAME') or 'postgres'
+    db_name: str = os.getenv('POSTGRES_DATABASE') or 'postgres'
 
     return f"postgresql+psycopg2://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
 
