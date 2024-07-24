de0fa2571b5ed74a74f0982777a5f7ed9327a258
Luganskii <luganchick@example.com>
2024-07-24 12:59:34 +0300

    change config

--- bot/config.py
+++ bot/config.py
@@ -1,3 +1,3 @@
 from networks.image_processor import ImageProcessor
 
-srresnet_x2_image_processor = ImageProcessor(config_path="/app/networks/srresnet.json")
+srresnet_x2_image_processor = ImageProcessor(config_path="/app/networks/srgan_x4.json")
